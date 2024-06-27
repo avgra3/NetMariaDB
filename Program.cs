e529d9affe7138789cdc46e2dd7ccee7b2c002a3
Antony <antonygradillas@gmail.com>
2024-06-27 14:09:25 -0700

    Added cli wrap to interact with mariadb

--- Program.cs
+++ Program.cs
@@ -1,23 +1,13 @@
-﻿using System;
+﻿using CliWrap;
+using CliWrap.Buffered;
+using System;
+using System.Text;
 using Tomlyn;
 using Tomlyn.Model;
 
 // Getting our toml file loaded
 string toml = File.ReadAllText("Config.toml");
 var model = Toml.ToModel(toml);
-
-// Server information
-Dictionary<string, object> serverInformation = new Dictionary<string, object>();
-
-foreach (var kvp in ((TomlTable)model["server"]!).ToDictionary())
-{
-		string key = kvp.Key.ToString();
-		object value = kvp.Value;
-		serverInformation.Add(key, value);
-}
-
-// Where the base binary is stored
-string? binDirectory = (((TomlTable)model["bin"]!)["path_to_exe"]).ToString();
 
 // Database information
 Dictionary<string, object> databaseInformation = new Dictionary<string, object>();
@@ -29,16 +19,48 @@ foreach (var kvp in ((TomlTable)model["database"]!).ToDictionary())
 		databaseInformation.Add(key, value);
 }
 
-Console.WriteLine("Loaded Server Information....");
-foreach (var pair in serverInformation)
-{
-		Console.WriteLine($"{pair.Key}: {pair.Value}");
-}
-
-Console.WriteLine($"\nLoaded Binary location: {binDirectory}");
-
-Console.WriteLine("\nLoaded Database Information....");
+Console.WriteLine("Loaded Database Information....");
 foreach (var pair in databaseInformation)
 {
 		Console.WriteLine($"{pair.Key}: {pair.Value}");
 }
+
+Console.WriteLine("Test execute...");
+string sqlTest = "SELECT * FROM bills_bill;";
+ExecuteCommand(databaseInformation, sqlTest);
+
+// Executing a sql command 
+void ExecuteCommand(Dictionary<string, object> databaseInformation, string sqlScript)
+{
+		var stdOutBuffer = new StringBuilder();
+		var stdErrBuffer = new StringBuilder();
+
+		var cmd = Cli.Wrap(databaseInformation["path_to_exe"].ToString())
+				.WithArguments(args =>
+						{
+								args.Add($"--host={databaseInformation["host"].ToString()}")
+							.Add($"--port={databaseInformation["port"]}")
+							.Add($"--user={databaseInformation["username"].ToString()}")
+							.Add($"--password={databaseInformation["password"].ToString()}");
+
+								// Optional
+								if (databaseInformation["allow_local_infile"] is bool)
+										args.Add("--local-infile=ON");
+
+								args.Add($"--database={databaseInformation["database"]}")
+							.Add("--verbose")
+							.Add("--column-names")
+							.Add($"--execute").Add(sqlScript);
+						})
+		.WithStandardOutputPipe(PipeTarget.ToStringBuilder(stdOutBuffer))
+			.WithStandardErrorPipe(PipeTarget.ToStringBuilder(stdErrBuffer))
+		.ExecuteAsync();
+
+		var stdOut = stdOutBuffer.ToString();
+		var stdErr = stdErrBuffer.ToString();
+
+		Console.WriteLine($"\nOutput of results:\n{stdOut}");
+		Console.WriteLine($"\nOutput of errors:\n{stdErr}");
+
+}
+
